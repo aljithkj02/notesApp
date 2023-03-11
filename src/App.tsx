cb76e668cf545750c6f8b3258db29590667e887a
Aljith KJ <kwdeathlok@gmail.com>
2023-03-11 21:09:11 +0530

    Basic react app created, AllRoutes added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+import './App.css';
+import AllRoutes from "./Routes/AllRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+};
+export default App;
