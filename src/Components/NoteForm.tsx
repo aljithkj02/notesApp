5900942ce2ff952170c766ee2cecec85d2e6232f
Aljith KJ <kwdeathlok@gmail.com>
2023-03-12 14:25:48 +0530

    Note, tag adding functionality added

--- src/Components/NoteForm.tsx
+++ src/Components/NoteForm.tsx
@@ -2,13 +2,16 @@ import React, { FormEvent, useRef, useState } from 'react'
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import CreatableReactSelect from 'react-select/creatable'
+import { v4 as uuidv4 } from 'uuid'
 import { Note, NoteData, Tag } from '../Routes/AllRoutes'
 
 interface INoteFrom {
     onSubmit: (data: NoteData) => void;
+    onAddTag: (tag: Tag) => void;
+    availableTags: Tag[];
 }
 
-const NoteForm = ({ onSubmit }: INoteFrom) => {
+const NoteForm = ({ onSubmit, onAddTag, availableTags }: INoteFrom) => {
     const titleRef = useRef<HTMLInputElement>(null);
     const markdownRef = useRef<HTMLTextAreaElement>(null);
     const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
@@ -18,7 +21,7 @@ const NoteForm = ({ onSubmit }: INoteFrom) => {
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: []
+            tags: selectedTags
         })
     }
     return (
@@ -38,11 +41,20 @@ const NoteForm = ({ onSubmit }: INoteFrom) => {
                             <Form.Label>
                                 Title
                             </Form.Label>
-                            <CreatableReactSelect value={selectedTags.map(tag => {
-                                return {
-                                    label: tag.label, value: tag.id
-                                }
-                            })}
+                            <CreatableReactSelect
+                                onCreateOption={label => {
+                                    const newTag = { id: uuidv4(), label };
+                                    onAddTag(newTag);
+                                    setSelectedTags(prev => [...prev, newTag]);
+                                }}
+                                value={selectedTags.map(tag => {
+                                    return {
+                                        label: tag.label, value: tag.id
+                                    }
+                                })}
+                                options={availableTags.map(tag => {
+                                    return { label: tag.label, value: tag.id }
+                                })}
                                 onChange={tags => {
                                     setSelectedTags(tags.map(tag => {
                                         return {
--- src/Pages/NewNote.tsx
+++ src/Pages/NewNote.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { NoteForm } from '../Components'
+import React from 'react';
+import { NoteForm } from '../Components';
+import { NoteData, Tag } from '../Routes/AllRoutes';
 
-const NewNote = () => {
+interface INewNote {
+    onSubmit: (data: NoteData) => void;
+    onAddTag: (tag: Tag) => void;
+    availableTags: Tag[]
+}
+
+const NewNote = ({ onSubmit, onAddTag, availableTags }: INewNote) => {
     return (
         <>
             <h1 className='mb-4'>New Note</h1>
-            <NoteForm />
+            <NoteForm onSubmit={onSubmit} onAddTag={onAddTag}
+                availableTags={availableTags}
+            />
         </>
     )
 }
